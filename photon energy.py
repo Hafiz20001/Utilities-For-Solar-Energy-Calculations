732b63a982c71de794be29c2415e4cd29c6ee5ea
Hafiz <83378921+Hafiz20001@users.noreply.github.com>
2022-06-25 17:43:51 +0300

    Add files via upload

---
+++ photon energy.py
@@ -0,0 +1,8 @@
+# Photon Energy Calculator
+
+wave = int(input("What is the value of the wave length?")) # asks user for the wave length value
+
+eV = 1.24 / wave # Photon energy equation at a particular wavelength expressed in electronic volts
+
+f"The photon energy at the particular wavelength is {eV} electronic volts" # print out the value of the photon energy
+
